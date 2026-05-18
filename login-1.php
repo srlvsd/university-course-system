3968e79a5386c219996b443daf541d8b410a8214
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-19 01:06:49 +0500

    Fix login: implement password_verify for hashed passwords

---
+++ login-1.php
@@ -0,0 +1,9 @@
+$user = $result->fetch_assoc();
+if (password_verify($password, $user['password'])) {
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['user_name'] = $user['name'];
+    header('Location: dashboard.php');
+    exit;
+} else {
+    $error = "Invalid password";
+}
