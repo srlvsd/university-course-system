eb8d65f95a6fc31bb294f5729136c68821b2d66b
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-19 23:31:39 +0500

    Add modules list to course page

--- course_upd.php
+++ course_upd.php
@@ -4,3 +4,4 @@ $modules_result = $conn->query($sql_modules);
 while($module = $modules_result->fetch_assoc()) {
     echo "<li><a href='module.php?id={$module['id']}'>{$module['title']}</a></li>";
 }
+
