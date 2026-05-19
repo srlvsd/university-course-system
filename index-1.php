117a18f7ea99450b0358e153a5e3f6f4a8bab523
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-19 23:17:27 +0500

    Add course listing query to index-1.php

---
+++ index-1.php
@@ -0,0 +1,6 @@
+$sql = "SELECT * FROM courses";
+$result = $conn->query($sql);
+
+while($course = $result->fetch_assoc()) {
+    echo "<li><a href='course.php?id={$course['id']}'>{$course['title']}</a></li>";
+}
