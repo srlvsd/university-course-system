a9815a51899fb6ab89f8b1cda2b7fdd3454748c0
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-19 23:16:47 +0500

    Add index.php with welcome message and links

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require_once 'config.php'; ?>
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Welcome to University</h1>
+    <a href="login.php">Login</a> | <a href="signup.php">Sign Up</a>
+</body>
+</html>
