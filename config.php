66e87c1fe441976b2b3e902f3f55f77048b98144
tairms <123245499+tairms@users.noreply.github.com>
2026-05-20 23:06:07 +0500

    Add session handling and homepage user greeting

--- config.php
+++ config.php
@@ -1,12 +1,21 @@
 <?php
+
+session_start();
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "test_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(
+    $servername,
+    $username,
+    $password,
+    $dbname
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
