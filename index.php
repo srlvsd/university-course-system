8fbbc069842b5f2f8546745ce35e37f0c3e55965
tairms <123245499+tairms@users.noreply.github.com>
2026-05-20 23:09:35 +0500

    Add homepage user greeting

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 <html>
 <body>
     <h1>Welcome to University</h1>
-    <a href="login.php">Login</a> | <a href="signup.php">Sign Up</a>
+    <?php if (isset($_SESSION['user_id'])): ?>
+
+<p>
+    Hello, <?php echo $_SESSION['user_name']; ?>!
+
+    <a href="dashboard.php">
+        Dashboard
+    </a> |
+
+    <a href="logout.php">
+        Logout
+    </a>
+</p>
+
+<?php else: ?>
+
+<p>
+    <a href="login.php">Login</a> |
+    <a href="signup.php">Sign Up</a>
+</p>
+
+<?php endif; ?>
 </body>
 </html>
