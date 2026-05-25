4ee94f9b0813c8d0dc5f295b50999226daff9210
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-25 23:08:20 +0500

    Implement user enrollment check in course update
    Added enrollment check for user in course.

--- course_upd.php
+++ course_upd.php
@@ -1,7 +1,20 @@
+<?php
+require_once 'config.php';
+
+$course_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+$sql = "SELECT * FROM courses WHERE id = $course_id";
+$course_result = $conn->query($sql);
+$course = $course_result->fetch_assoc();
+
 $sql_modules = "SELECT * FROM modules WHERE course_id = $course_id";
 $modules_result = $conn->query($sql_modules);
 
-while($module = $modules_result->fetch_assoc()) {
-    echo "<li><a href='module.php?id={$module['id']}'>{$module['title']}</a></li>";
+$is_enrolled = false;
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $check_sql = "SELECT * FROM user_courses WHERE user_id = $user_id AND course_id = $course_id";
+    $check_result = $conn->query($check_sql);
+    $is_enrolled = $check_result->num_rows > 0;
 }
-
+?>
