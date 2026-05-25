c63f3b54dabecbfaf974fe6c4b471cd9e4fa63d7
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-25 23:19:31 +0500

    Fetch courses from database in index.php
    Added PHP code to fetch courses from the database.

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php require_once 'config.php'; ?>
-<!DOCTYPE html>
-<html>
-<body>
-    <h1>Welcome to University</h1>
-    <a href="login.php">Login</a> | <a href="signup.php">Sign Up</a>
-</body>
-</html>
+<?php
+require_once 'config.php';
+
+// Get all courses from the database
+$sql = "SELECT * FROM courses";
+$result = $conn->query($sql);
+?>
