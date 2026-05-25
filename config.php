5788f82683d124f8596184dcd0cb067ce19636ca
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-25 23:05:19 +0500

    Change database name and update error message case

--- config.php
+++ config.php
@@ -2,11 +2,13 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test_db";
+$dbname = "university_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("connection failed: " . $conn->connect_error);
 }
+    
+session_start();
 ?>
