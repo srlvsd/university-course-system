f7ff319729605ae9157bf8ef07c86a12f1ae0628
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2026-05-25 23:08:49 +0500

    Add HTML structure for course details page

--- course_upd.php
+++ course_upd.php
@@ -18,3 +18,47 @@ if (isset($_SESSION['user_id'])) {
     $is_enrolled = $check_result->num_rows > 0;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $course['title']; ?> - University Courses</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p><a href="index.php">← Back to Home</a></p>
+    
+    <h1><?php echo $course['title']; ?></h1>
+    <p><?php echo $course['description']; ?></p>
+    
+    <h2>Course Modules</h2>
+    
+    <?php if ($modules_result->num_rows > 0): ?>
+        <ul>
+            <?php while($module = $modules_result->fetch_assoc()): ?>
+                <li>
+                    <a href="module.php?id=<?php echo $module['id']; ?>">
+                        <?php echo $module['code']; ?> - <?php echo $module['title']; ?>
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php else: ?>
+        <p>No modules available yet.</p>
+    <?php endif; ?>
+    
+    <!-- Enrol button (only for logged in users) -->
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <?php if ($is_enrolled): ?>
+            <p>✅ You are already enrolled in this course</p>
+        <?php else: ?>
+            <a href="register_course.php?course_id=<?php echo $course_id; ?>" class="button">
+                Enrol in this course
+            </a>
+        <?php endif; ?>
+    <?php else: ?>
+        <p><a href="login.php">Login</a> to enrol in this course</p>
+    <?php endif; ?>
+</body>
+</html>
